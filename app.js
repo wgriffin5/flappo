43c00e83e828e8ef080329234722cf5982d9c69f
William Griffin <williamgriffin989@gmail.com>
2015-04-15 14:37:12 -0400

    ordered list

---
+++ app.js
@@ -0,0 +1,21 @@
+angular.module('flappoNews', [])
+.controller('MainCtrl', [
+
+'$scope',
+function($scope) {
+  $scope.test = 'Buenos dias mundo';
+
+
+
+$scope.posts = [
+
+  {title: 'post 1', upvotes: 3},
+  {title: 'post 2', upvotes: 2},
+  {title: 'post 3', upvotes: 10},
+  {title: 'post 4', upvotes: 5},
+  {title: 'post 5', upvotes: 14}
+  
+];
+
+
+}]);
