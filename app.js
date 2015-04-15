55b9e1644308745af1d9e8174aa7d7bb929ff667
William Griffin <williamgriffin989@gmail.com>
2015-04-15 16:05:33 -0400

    new posts with validation

--- app.js
+++ app.js
@@ -3,19 +3,30 @@ angular.module('flappoNews', [])
 
 '$scope',
 function($scope) {
-  $scope.test = 'Buenos dias mundo';
+  // $scope.test = 'Buenos dias mundo';
 
 
 
-$scope.posts = [
+  $scope.posts = [
 
-  {title: 'post 1', upvotes: 3},
-  {title: 'post 2', upvotes: 2},
-  {title: 'post 3', upvotes: 10},
-  {title: 'post 4', upvotes: 5},
-  {title: 'post 5', upvotes: 14}
-  
-];
+    {title: 'post 1', upvotes: 3},
+    {title: 'post 2', upvotes: 2},
+    {title: 'post 3', upvotes: 10},
+    {title: 'post 4', upvotes: 5},
+    {title: 'post 5', upvotes: 14}
+    
+  ];
+
+
+
+
+
+$scope.addPost = function(){
+  if(!$scope.title || $scope.title === '') { return; }
+  $scope.posts.push({title: $scope.title, upvotes: 0});
+  $scope.title = '';
+};
+
 
 
 }]);
