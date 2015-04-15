28e1f33e609dc58093b982e9f7823e955922667c
William Griffin <williamgriffin989@gmail.com>
2015-04-15 19:19:11 -0400

    begun routing

--- app.js
+++ app.js
@@ -1,32 +1,115 @@
-angular.module('flappoNews', [])
+angular.module('flappoNews', ['ui.router'])
+
+.config([ 
+'$stateProvider',
+'@urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html', 
+      controller: 'MainCtrl'
+      });
+    })
+
+  $urlRouterProvider.otherwise('home');
+
+
+  .state('posts', {
+    url: '/posts/{id}',
+    templateUrl: '/posts.html',
+    controller: 'PostsCtrl'
+  });
+
+}])
+
+
+
+
+  .factory('posts', [function(){
+
+    var o = {
+
+      posts: []
+
+    };
+    return o;
+  }])
+
+
 .controller('MainCtrl', [
 
 '$scope',
-function($scope) {
+'posts',
+function($scope, posts) {
   // $scope.test = 'Buenos dias mundo';
 
 
 
-  $scope.posts = [
+  $scope.posts = posts.posts;
 
-    {title: 'post 1', upvotes: 3},
-    {title: 'post 2', upvotes: 2},
-    {title: 'post 3', upvotes: 10},
-    {title: 'post 4', upvotes: 5},
-    {title: 'post 5', upvotes: 14}
+
+  // [
+
+  //   {title: 'post 1', upvotes: 3},
+  //   {title: 'post 2', upvotes: 2},
+  //   {title: 'post 3', upvotes: 10},
+  //   {title: 'post 4', upvotes: 5},
+  //   {title: 'post 5', upvotes: 14}
     
-  ];
-
+  // ];
+}
 
 
 
 
 $scope.addPost = function(){
   if(!$scope.title || $scope.title === '') { return; }
-  $scope.posts.push({title: $scope.title, upvotes: 0});
+  $scope.posts.push({
+
+    title: $scope.title, 
+    link: $scope.link,
+    upvotes: 0
+  });
+
   $scope.title = '';
+  $scope.link = '';  
 };
 
 
 
+
+$scope.incrementUpvotes = function(post) {
+  post.upvotes += 1;
+};
+
+
 }]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
